3cff04c2ceacc02006d2f7afb293a2eeab738d94
Kanturna <k4nturna@gmail.com>
2026-04-19 19:40:24 +0200

    fix: run_tests.bat funktioniert wieder

--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,3 @@
 @echo off
-"D:\Programme\Godot\godot_console.exe" --path "%~dp0" --headless --script res://src/tests/test_runner.gd --quit
+"D:\Programme\Godot\godot_console.exe" --path "%~dp0." --headless --script res://src/tests/test_runner.gd --quit
 exit /b %ERRORLEVEL%
